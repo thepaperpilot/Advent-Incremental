57f5da94527eb8f42f6ffc7d0973d6b6fb53db15
unsoftcapped3 <75136164+unsoftcapped3@users.noreply.github.com>
2022-12-15 03:28:03 +0000

    more scaling

--- src/data/layers/management.tsx
+++ src/data/layers/management.tsx
@@ -1454,7 +1454,7 @@ const layer = createLayer(id, () => {
 
     const classroomCost = computed(() => {
         var v = classrooms.amount.value;
-        if(Decimal.gte(v,100)) v=Decimal.pow(v,1.5).div(10)
+        if(Decimal.gte(v,100)) v=Decimal.pow(v,2).div(100)
         const classroomFactor = Decimal.add(v, 1).pow(1.5);
         return {
             wood: classroomFactor.mul(1e21),
